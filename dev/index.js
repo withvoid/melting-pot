00e64e5ba49ae56f5661a0f29ada514f6430c364
adeelibr <adeelimranr@gmail.com>
2019-01-09 23:38:00 +0500

    Added dev server base way of usage

--- dev/index.js
+++ dev/index.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// @withvoid/melting-pot components
-// import useWindowSize from '../src/useWindowSize';
-import { useWindowSize } from '../src';
 
-const styles = {
-  root: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: 'yellow',
-  },
-  button: {
-    margin: '0 15px',
-    padding: 15,
-    height: 40,
-    backgroundColor: 'teal',
-    color: '#fff',
-    border: 0,
-  },
-};
+// Components (Use one at a time)
+import Faces from '../examples/useKeyPress/Faces';
 
 const App = () => {
-  const size = useWindowSize();
   return (
-    <div style={styles.root}>
-      <p>{JSON.stringify(size)}</p>
+    <div>
+      <Faces />
     </div>
   );
 };
