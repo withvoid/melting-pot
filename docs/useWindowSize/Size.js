5dd2544721a474e302733e7eb0811d85232443ba
adeelibr <adeelimranr@gmail.com>
2019-01-10 23:53:41 +0500

    added demo on useWindowSize

--- docs/useWindowSize/Size.js
+++ docs/useWindowSize/Size.js
@@ -1,62 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // Components
 import Emoji from '../components/Emoji';
-import { useWindowSize } from '../../src';
 
 const styles = {
   root: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: 'teal',
+    backgroundColor: '#B61173',
   },
   paragraph: {
     margin: 10,
     padding: 0,
-    fontSize: 21,
+    fontSize: 28,
     color: '#fff',
     textAlign: 'center',
   },
   empashis: {
-    color: 'yellow',
-  },
-  bigParagraph: {
-    margin: 10,
-    padding: 0,
-    fontSize: 32,
-    color: '#fff',
-    textAlign: 'center',
+    color: '#000',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
   },
 };
 
-const Size = () => {
-  const { width, height } = useWindowSize();
+const Size = ({ width, height }) => {
   return (
     <div style={styles.root}>
       <p style={styles.paragraph}>
+        <Emoji emoji="🤪🤪🤩" />
+        Width <span style={styles.empashis}>{width} px</span>
         <Emoji emoji="🤩" />
-        Width <span style={styles.empashis}>{width}px</span>
-        <Emoji emoji="🤩" />
+        Height <span style={styles.empashis}>{height} px</span>
+        <Emoji emoji="🤩🤪🤪" />
       </p>
       <p style={styles.paragraph}>
-        <Emoji emoji="🤪" />
-        Height <span style={styles.empashis}>{height}px</span>
-        <Emoji emoji="🤪" />
-      </p>
-
-      <p style={styles.bigParagraph}>
         <Emoji emoji="✌🏻" />
-        Drag The Window Size & Watch The Emoji Change At 900px
+        Resize Browser & Watch The Emoji Change At 800px
         <Emoji emoji="✌🏻" />
       </p>
-      <p style={styles.bigParagraph}>
+      <p style={styles.paragraph}>
         {[...Array(8).keys()].map(i => (
-          <Emoji key={i} emoji={width <= 900 ? '🐶' : '🦅'} />
+          <Emoji key={i} emoji={width <= 800 ? '🐶' : '🦅'} />
         ))}
       </p>
     </div>
   );
+};
+
+Size.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
 };
 
 export default Size;
