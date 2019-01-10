bf63bbff13455bb58766dd125cb44d26327f85ab
adeelibr <adeelimranr@gmail.com>
2019-01-10 22:56:12 +0500

    Upgraded babel, added .docz to gitignore & installed docz & docz-theme-default and upgraded react to 16.8.0-alpha-0

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Melting Point',
+  description: 'A utility library for daily use.',
+  source: './docs',
+  debug: false,
+  theme: 'docz-theme-default',
+  themeConfig: {
+    mode: 'dark',
+    colors: {
+      primary: 'tomato',
+    },
+  },
+};
