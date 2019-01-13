2bc3ade0aa482637bc374ea9c54cc0ea1cfdb33a
adeelibr <adeelimranr@gmail.com>
2019-01-13 22:30:27 +0500

    added netlfiy plugin

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,24 @@
+import fs from 'fs';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createPlugin } from 'docz-core';
+/**
+ * import { createPlugin } from 'docz-core';
+ * 'docz-core' is a peer-dependecy of docz
+ *
+ * Reference for doczPluginNetlify
+ * https://github.com/nicholasess/docz-plugin-netlify
+ */
+
+const doczPluginNetlify = () => {
+  createPlugin({
+    onPostBuild: config => {
+      fs.writeFileSync(`./${config.dest}/_redirects`, '/*    /index.html   200', {
+        encoding: 'utf8',
+      });
+    },
+  });
+};
+
 export default {
   title: 'Melting Point',
   description: 'A utility library for daily use.',
@@ -10,4 +31,5 @@ export default {
       primary: 'tomato',
     },
   },
+  plugins: [doczPluginNetlify()],
 };
