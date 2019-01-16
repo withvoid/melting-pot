ed74bbb0f47ec69f04917fe8036866204ddfa68b
adeelibr <adeelimranr@gmail.com>
2019-01-16 23:21:29 +0500

    Added documentation for useFormField

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as useWindowSize } from './hooks/useWindowSize';
 export { default as useKeyPress } from './hooks/useKeyPress';
 export { default as useOnlineStatus } from './hooks/useOnlineStatus';
 export { default as useDidMount } from './hooks/useDidMount';
+export { default as useFormField } from './hooks/useFormField';
