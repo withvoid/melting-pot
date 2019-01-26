e3821dfd4c77df0bc311d525851183063445c242
adeelibr <adeelimranr@gmail.com>
2019-01-26 23:04:44 +0500

    updated rollup configuration'

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 
 const dist = 'dist';
-const getOutputFileByFormatType = type => `${dist}/bundle.${type}.js`;
+const getOutputFileByFormatType = type => `${dist}/melting-pot.${type}.js`;
 
 export default {
   input: 'src/index.js',
@@ -16,9 +16,12 @@ export default {
       format: 'esm', // same as 'es' (shorthand)
     },
     {
-      name: 'TheHooks',
+      name: 'MeltingPot',
       file: getOutputFileByFormatType('umd'),
       format: 'umd',
+      globals: {
+        react: 'React',
+      },
     },
   ],
   plugins: [resolve()],
