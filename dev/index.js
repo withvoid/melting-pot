2e55b5820f862c0ff812fbdda3b5bc99a3775677
Adeel Imran <adeelimranr@gmail.com>
2019-01-27 17:41:04 +0500

    [Feature] Add a transducer function for Strings (#10)
    Added a string utility that takes in an array of arguments & pipes them one by one. (Transducer concept)

--- dev/index.js
+++ dev/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { useWindowSize } from '../src';
+import { pipe } from '../src';
 
 const App = () => {
-  const { width } = useWindowSize();
+  const onCapital = (string = '') => string.toUpperCase();
+  const onRemoveSpaces = (string = '') => string.replace(' ', '');
+  const onAppendSymbol = symbol => string => {
+    return `${string}${symbol}`;
+  };
+  const onPreAppendSymbol = symbol => string => {
+    return `${symbol}${string}`;
+  };
+
   return (
     <div>
-      <p>width is {width}px.</p>
+      <p>{onAppendSymbol('@')(onRemoveSpaces(onCapital('Adeel Imran')))}</p>
+      <p>
+        {pipe({
+          value: 'Adeel Imran',
+          toPipe: [onCapital, onRemoveSpaces, onAppendSymbol('!'), onPreAppendSymbol('==> ')],
+        })}
+      </p>
     </div>
   );
 };
