ba18231b9e80104fedfed391efd57fde986a4275
Afaq Khan <afaq.qwe@gmail.com>
2019-01-30 16:04:46 +0500

    work

--- dev/index.js
+++ dev/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { useWindowSize } from '../src';
+import { useWindowSize, useToggle  } from '../src';
 
 const App = () => {
+  const isOn = true;
   const { width } = useWindowSize();
+  const {on, toggle} = useToggle(isOn);
+
   return (
     <div>
       <p>width is {width}px.</p>
+      <button onClick={toggle}>
+        {on ? 'on' : 'off'}
+      </button>
     </div>
   );
 };
