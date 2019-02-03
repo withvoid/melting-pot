8861f3467408c21b77f5f6af2996e7c597b14661
adeelibr <adeelimranr@gmail.com>
2019-02-04 00:27:08 +0500

    Updated rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -6,15 +6,19 @@ import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', 'prop-types'],
   output: [
     { file: pkg.main, format: 'cjs', exports: 'named' },
     { file: pkg.module, format: 'es', exports: 'named' },
   ],
   plugins: [
     resolve(),
-    babel({ exclude: 'node_modules/**' }),
-    commonjs({ include: 'node_modules/**' }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
     terser(),
   ],
 };
