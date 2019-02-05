c78a9721c5a5f45ab58dd7244a5c5759c37e07a1
Adeel Imran <adeelimranr@gmail.com>
2019-02-05 17:20:49 +0500

    Enhance/useformfield improve (#25)
    * improved useFormField
    * added PropsTable component
    * changed docz theme

--- src/hooks/useFormField.js
+++ src/hooks/useFormField.js
@@ -2,19 +2,42 @@ import React from 'react';
 
 const useFormField = (initialValue = '') => {
   const [value, set] = React.useState(initialValue);
+  const [isDirty, setDirty] = React.useState(false);
+  const [isSubmitted, setSubmitted] = React.useState(false);
 
-  const isEmpty = () => {
-    if (typeof value === 'string') {
-      return value.trim().length === 0;
-    }
-    return value === undefined || value === null;
+  React.useEffect(
+    () => {
+      if (value.length > 0) {
+        setDirty(true);
+      }
+    },
+    [value],
+  );
+
+  const reset = () => {
+    set(initialValue);
+    setDirty(false);
+    setSubmitted(false);
   };
+
+  const validate = () => {
+    setDirty(true);
+    setSubmitted(true);
+  };
+
+  const isEmpty = value.length === 0;
+
+  const isValid = (isEmpty && isDirty && isSubmitted) || (isEmpty && isDirty);
 
   return {
     value,
     set,
-    reset: () => set(initialValue),
+    reset,
+    validate,
     isEmpty,
+    isDirty,
+    isSubmitted,
+    isValid,
     bind: {
       value,
       onChange: event => set(event.target.value),
