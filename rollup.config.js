d588824fc2e79b066d71a71e4f99868af4fd6b50
adeelibr <adeelimranr@gmail.com>
2019-02-05 21:21:47 +0500

    Added size snapshot

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,22 @@
 import resolve from 'rollup-plugin-node-resolve';
-import autoExternal from 'rollup-plugin-auto-external';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  // external: ['react', 'react-dom', 'prop-types'], // see: autoExternal()
+  external: ['react', 'react-dom', 'prop-types'],
   output: [
     { file: pkg.main, format: 'cjs', exports: 'named' },
     { file: pkg.module, format: 'es', exports: 'named' },
   ],
   plugins: [
     resolve(),
-    autoExternal(), // will not bundle dependencies | peerDependencies
     commonjs({ include: 'node_modules/**' }),
     babel({ exclude: 'node_modules/**' }),
     terser(),
+    sizeSnapshot(),
   ],
 };
