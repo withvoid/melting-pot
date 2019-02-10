bafd052394409ae9c09e465c4d198e091490fad9
Umair Farooq <umairfarooq44@gmail.com>
2019-02-10 15:32:59 +0500

    added example

--- src/hooks/useUpdate.js
+++ src/hooks/useUpdate.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const useUpdate = ({ action, exit }, ...args) => {
-  const effect = () => {
-    if (action) {
-      action();
-    }
-    return exit;
-  };
-  if (args.length) {
-    return React.useEffect(effect, [...args]);
-  }
-  return React.useEffect(effect);
+const useUpdate = ({ action = () => {}, exit = () => {} }, ...args) => {
+  React.useEffect(
+    () => {
+      if (action && typeof action === 'function') {
+        action();
+      }
+      return exit;
+    },
+    args.length ? [...args] : undefined,
+  );
 };
 
 export default useUpdate;
