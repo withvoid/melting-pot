cbb933cd4b47e981f6afc1143ce9401e27a2ef70
Sania Jamil <sania.jamil@venturedive.com>
2019-02-11 11:40:19 +0500

    ran prettier

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
 const useTitle = title => {
-  React.useEffect(
-    () => {
-      document.title = title;
-    },
-    [title],
-  );
+  React.useEffect(() => {
+    document.title = title;
+  }, [title]);
 };
 
 export default useTitle;
