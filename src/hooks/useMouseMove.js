2f9c011a3efc1176de37421c1d35565c753bacdb
adeelibr <adeelimranr@gmail.com>
2019-02-19 02:06:58 +0500

    Improved useMouseMove api

--- src/hooks/useMouseMove.js
+++ src/hooks/useMouseMove.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
-const useMouseMove = () => {
+const useMouseMove = ({ isWindow = false } = {}) => {
   const [coords, setCoords] = React.useState({ x: 0, y: 0 });
 
+  React.useEffect(() => {
+    const onSetWindowCoords = event => {
+      setCoords({ x: event.screenX, y: event.screenY });
+    };
+    if (isWindow) {
+      window.addEventListener('mousemove', onSetWindowCoords);
+    }
+    return () => {
+      if (isWindow) {
+        window.removeEventListener('mousemove', onSetWindowCoords);
+      }
+    };
+  }, []);
+
+  /*
+   * @todo: get x & y axis respective of the HTML div element with which
+   * the element has been binded with.
+   */
+  const onSetElementCoords = event => {
+    setCoords({ x: event.screenX, y: event.screenY });
+  };
+
   return {
-    ...coords,
-    bind: {
-      onMouseMove: event => {
-        setCoords({ x: event.screenX, y: event.screenY });
+    x: coords.x,
+    y: coords.y,
+    ...(!isWindow && {
+      ...{
+        bind: { onMouseMove: onSetElementCoords },
       },
-    },
+    }),
   };
 };
 
